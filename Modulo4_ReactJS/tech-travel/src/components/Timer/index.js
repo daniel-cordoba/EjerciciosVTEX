50c5f5cbbeb7e28c8a94ddbab30332e4ac28dddc
Daniel Cordoba Arias <daniel.cordoba13@gmail.com>
2022-05-11 22:23:16 -0500

    Modulo4 - ​​Lección 10 - Estado y Ciclo de Vida

--- Modulo4_ReactJS/tech-travel/src/components/Timer/index.js
+++ Modulo4_ReactJS/tech-travel/src/components/Timer/index.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 
 class Timer extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    componentDidMount(){
+        this.timer = setInterval(() => this.tick(), 2000 )
+    };
+
+    componentWillUnmount(){
+        clearInterval(this.timer);
+    }
+
+    tick(){
+        this.setState({
+            count: this.state.count + 1
+        });
+    };
+
     render(){
         return(
-            <p>0</p>
+            <p>{this.state.count}</p>
         )
     }
 }
