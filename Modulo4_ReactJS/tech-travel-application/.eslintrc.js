c04dafbce2a1b7ca76436659c6cce75b3f374380
Daniel Cordoba Arias <daniel.cordoba13@gmail.com>
2022-05-18 13:48:18 -0500

    Modulo4 - Lección15 - Instalación ESLint/Prettier

---
+++ Modulo4_ReactJS/tech-travel-application/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'no-unused-expressions': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-console': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/props-types': 'off',
+    'react/jsx-no-bind': 'off',
+    'prettier/prettier': 'error',
+  },
+};
