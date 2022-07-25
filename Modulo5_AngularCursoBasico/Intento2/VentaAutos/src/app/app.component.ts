40295a30fb3b22cb59e8808ed978edf1598b264a
Daniel Cordoba Arias <daniel.cordoba13@gmail.com>
2022-07-24 23:11:33 -0500

    Leccion5 - Componentes

--- Modulo5_AngularCursoBasico/Intento2/VentaAutos/src/app/app.component.ts
+++ Modulo5_AngularCursoBasico/Intento2/VentaAutos/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html"
 })
+
 export class AppComponent {
-  title = 'VentaAutos';
+  titulo: string = "Venta de Autos";
+
+  metodoPrueba() : void {
+
+  }
 }
