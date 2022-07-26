8823a8487fbb0862427682d247bf7d5ebb1b48f4
Daniel Cordoba Arias <daniel.cordoba13@gmail.com>
2022-07-25 22:44:48 -0500

    Leccion7 - Enlace de propiedades

--- Modulo5_AngularCursoBasico/Intento2/VentaAutos/src/app/autos/lista-autos/lista-autos.component.ts
+++ Modulo5_AngularCursoBasico/Intento2/VentaAutos/src/app/autos/lista-autos/lista-autos.component.ts
@@ -1,10 +1,80 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "lista-autos",
-    templateUrl: "./lista-autos.component.html"
+    templateUrl: "./lista-autos.component.html",
+    styleUrls: ['./lista-autos.component.css']
 })
-export class ListaAutosComponent {
-        tituloListaAutos: string = "Lista de Automóviles";
-        arregloAutos: string[] = ["uno"]
+
+export class ListaAutosComponent implements OnInit {
+    tituloListaAutos: string = "Lista de Automóviles";
+    listaAutos: Auto[] = [];
+    filtro: string = 'default';
+    imageWidth = 200;
+    imageMargin = 2;
+    muestraImagen: boolean = false;
+
+    ngOnInit(): void {
+        this.listaAutos = [
+            {
+                id: 1,
+                imagenUrl: "../../assets/KiaCerato.jpeg",
+                marca: "Kia",
+                modelo: "Cerato Vivro",
+                anio: 2022,
+                color: "Gris Oscuro",
+                kilometros: 9500,
+                calificacion: 4.2,
+                precio: 90000000,
+            },
+            {
+                id: 2,
+                imagenUrl: "../../assets/Mazda3.jpg",
+                marca: "Mazda",
+                modelo: "3 Touring",
+                anio: 2021,
+                color: "Gris Plata",
+                kilometros: 6500,
+                calificacion: 4.8,
+                precio:96000000,
+            },
+            {
+                id: 3,
+                imagenUrl: "../../assets/NissanSentra.jpg",
+                marca: "Nissan",
+                modelo: "Sentra SR",
+                anio: 2022,
+                color: "Naranja Bi-tono",
+                kilometros: 12000,
+                calificacion: 4.4,
+                precio: 110000000,
+            },
+            {
+                id: 4,
+                imagenUrl: "../../assets/ToyotaCorolla.jpg",
+                marca: "Toyota",
+                modelo: "Corolla Seg",
+                anio: 2020,
+                color: "Azul Oscuro",
+                kilometros: 21500,
+                calificacion: 4.7,
+                precio: 105000000,
+            },
+            {
+                id: 5,
+                imagenUrl: "../../assets/VolkswagenJetta.jpg",
+                marca: "Volkswagen",
+                modelo: "Jetta Highline",
+                anio: 2022,
+                color: "Rojo",
+                kilometros: 4000,
+                calificacion: 4.9,
+                precio: 108000000,
+            }
+        ];
+    }
+
+    muestraImagenes(): void {
+        this.muestraImagen = !this.muestraImagen;
+    }
 }
